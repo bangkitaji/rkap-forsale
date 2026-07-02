52f0924fecae252751dd6ff46b269ed890da530f
bangkitaji <a.bangkit.n@gmail.com>
2026-07-02 17:09:33 +0700

    remove un implemented ai chat widget

--- resources/views/layouts/contentNavbarLayout.blade.php
+++ resources/views/layouts/contentNavbarLayout.blade.php
@@ -86,6 +86,6 @@ $container = ($container ?? 'container-xxl');
         <div class="drag-target"></div>
     </div>
     <!-- / Layout wrapper -->
-    <livewire:ai-chat-widget />
+    <!-- <livewire:ai-chat-widget /> -->
 </div>
 @endsection
